905060530030fd46e95f2c9f48ecd6ebbc153a13
Artur <arturusatyukl@gmail.com>
2024-02-01 20:28:44 +0200

    створив додаток для оцінки фільмів

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,45 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    'env': {
+        'browser': true,
+        'es2021': true
     },
-    "extends": "eslint:recommended",
-    "overrides": [
+    'extends': 'eslint:recommended',
+    'overrides': [
         {
-            "env": {
-                "node": true
+            'env': {
+                'node': true
             },
-            "files": [
-                ".eslintrc.{js,cjs}"
+            'files': [
+                '.eslintrc.{js,cjs}'
             ],
-            "parserOptions": {
-                "sourceType": "script"
-            }
-        }
+            'parserOptions': {
+                'sourceType': 'script'
+            },
+        },
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
     },
-    "rules": {
+    'rules': {
+        'indent': [
+            'warn',
+        ],
+        'linebreak-style': [
+            'error',
+            'windows'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'no-unused-vars': [
+            'warn'
+        ]
     }
-}
+};
+
--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-123
+const numberOfFilms = +prompt('Скільки фільмів ви вже переглянули?', '');
+
+const personslMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один з останніх переглянутих фільмів?',''),
+    b = prompt('На скільки оцінете його?',''),
+    c = prompt('Один з останніх переглянутих фільмів?',''),
+    d = prompt('На скільки оцінете його?','');
+
+personslMovieDB.movies[a] = b;
+personslMovieDB.movies[c] = d;
+
+console.log(personslMovieDB);
